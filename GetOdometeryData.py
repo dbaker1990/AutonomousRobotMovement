8f3410e908dcb3dbac5102dbb35fd97cec478fa1
dbaker1990 <dominicbaker16@gmail.com>
2023-07-03 14:22:37 -0400

    Add files via upload

--- GetOdometeryData.py
+++ GetOdometeryData.py
@@ -16,16 +16,77 @@ from rosbags.image import message_to_cvimage
 from PIL import Image as im
 import cv2
 import tf
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
+import tf2_py as tf2
+import pandas as pd
 
 
 fileName = "upstairs_hallway_and_office_1_robot_10"
-directory = "/home/dominic/Downloads/Project/RosBags/"
+directory = "./RosBags/"
 results = directory + fileName
 extension = ".bag"
 bag = rosbag.Bag(directory+fileName+extension)
 
-for topic, msg, t in bag.read_messages(topics=['/tf']):
-     #print(listener.subscription_callback(msg))
-     info = msg._has_header
-     
-print(dir(info))
+#Important Lists
+translationXList = []
+translationYList = []
+translationZList = []
+rotationXList = []
+rotationYList = []
+rotationZList = []
+rotationWList = []
+timestamp = []
+
+tfListener = tf.Transformer(True, rospy.Duration(3600.0))
+
+def GetBasicInformation(fileNames: str, directorys: str):
+    fileName = fileNames
+    directory = directorys
+    results = directory + fileName + extension
+    bag = rosbag.Bag(directory+fileName+extension)
+    GetTimeStamps()
+    
+
+def GetMapToOdomTranslationAndRotation():
+    for topic, msg, t in bag.read_messages(topics=['/tf']):
+        for msg_tf in msg.transforms:
+            helping = msg_tf
+            translationX = msg_tf.transform.translation.x
+            translationXList.append(translationX)
+            translationY = msg_tf.transform.translation.y
+            translationYList.append(translationY)
+            translationZ = msg_tf.transform.translation.z
+            translationZList.append(translationZ)
+            rotationX = msg_tf.transform.rotation.x
+            rotationXList.append(rotationX)
+            rotationY = msg_tf.transform.rotation.y
+            rotationYList.append(rotationY)
+            rotationZ = msg_tf.transform.rotation.z
+            rotationZList.append(rotationZ)
+            rotationW = msg_tf.transform.rotation.w
+            rotationWList.append(rotationW)
+    bag.close()
+
+def GetTimeStamps():
+    for topic, msg, t in bag.read_messages(topics=['/tf']):
+        timestamp.append(t.to_sec())
+
+#Get all the infromation from the translation and rotation and put it inside of a csv file
+def AddInformationToCSVFile(translationX: list, translationY: list, translationZ: list, rotationX: list, rotationY: list, rotationZ: list, rotationW: list):
+    csvFileName = "OdomInformation.csv"
+    fields = ["transX", "transY", "transZ", "rotX", "rotY", "rotZ", "rotW"]
+    columns = {}
+    columns["transx"] = translationXList
+    columns["transy"] = translationYList
+    columns["transz"] = translationZList
+    columns["rotx"] = rotationXList
+    columns["roty"] = rotationYList
+    columns["rotz"] = rotationZList
+    columns["rotw"] = rotationWList
+    rows = zip(columns["transx"],columns["transy"],columns["transz"],columns["rotx"],columns["roty"],columns["rotz"], columns["rotw"])
+    
+    with open(csvFileName, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(fields)
+        csvwriter.writerows(rows)
--- Main.py
+++ Main.py
@@ -1,34 +1,50 @@
-import torch
-import numpy as np
-from datetime import datetime
 import sys
-import os
-import LidarDataset
-import TrainingModule
-import ImageProcessing
 import FolderCheck
 import Handle_ROS_Files
 from pathlib import Path
 import DistanceConversion
 import ConvertBagVideoToImages
 import GetLidarInformation
-    
-#checks to see if you have the neccessary paths
+import GetOdometeryData
+
+#checks to see if you have the neccessary paths. Do not Delete this. It detects all neccesarry folders that is needed for this project to run
 a =  FolderCheck.CheckIfNecessaryDirectoryExists()
 
+#if all the folders exists do nothing. But it tells you the first folder it is missing and then closes the program
 if a == True:
     print()
 else:
     sys.exit(0)
 
+
 #Directory and common variables in Project. So it can be modular
 directory = "./RosBags/"
 filenameToConvert = "upstairs_hallway_and_office_1_robot_10"
 robotName = "sekhmet"
+#These are optional variables. They are only good for certain function calls.s
+count = 0
+rosBagList = []
 
 #convert bag videos to images and turns them into tensor grayscale
-ConvertBagVideoToImages.conversion(robotName, filenameToConvert, directory)
+#ConvertBagVideoToImages.conversion(robotName, filenameToConvert, directory)
 
 #Create the CSV file for Lidar Data
-GetLidarInformation.GetLidarInfo(directory,filenameToConvert,robotName)
+#GetLidarInformation.GetLidarInfo(directory,filenameToConvert,robotName)
+
+#If needed this gets all the Ros Bags that's in the directory. Just uncomment if needed
+#Handle_ROS_Files.GetBagsFromDirectory(directory, rosBagList)
+#print(rosBagList[1])
+#for i in rosBagList:
+#    print(rosBagList[count])
+#    count = count + 1
+
+#This just calls the Distance Conversion file. Add the . to see all the functions that you possible would need
+#DistanceConversion
+
+GetOdometeryData.fileName = "upstairs_hallway_and_office_1_robot_10"
+GetOdometeryData.GetMapToOdomTranslationAndRotation()
+print(GetOdometeryData.translationXList[1])
+print(GetOdometeryData.translationYList[1])
+print(GetOdometeryData.translationZList[1])
+#GetOdometeryData.AddInformationToCSVFile(GetOdometeryData.translationXList,GetOdometeryData.translationYList, GetOdometeryData.translationZList,GetOdometeryData.rotationXList,GetOdometeryData.rotationYList,GetOdometeryData.rotationZList,GetOdometeryData.rotationWList)
 
